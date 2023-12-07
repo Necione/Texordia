44406a061b87675bb66873cf379288a51590450a
Necione <69877102+Necione@users.noreply.github.com>
2023-12-06 20:01:21 -0800

    a

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 app.use(express.static('public')); // Serve static files from 'public' directory
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
